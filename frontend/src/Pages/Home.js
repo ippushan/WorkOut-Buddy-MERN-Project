d83b40ec8ab409558867911702b278a9909011c7
Moahmmed Shan S <mohammedshanshajahan@gmail.com>
2022-12-19 16:47:52 +0530

    nothing to change

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -14,6 +14,7 @@ const Home = () => {
             
             const json = await response.json()
             
+            
             if(response.ok){
                 dispatch({ type : "SET_WORKOUTS", payload: json})
             }
